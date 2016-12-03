ae8bbb5329ca530ea7a53e5e22f766fabc20d9f0
Callan Morrison <callan.l.morrison@gmail.com>
2016-12-03 11:49:34 -0600

    init boilerplate

---
+++ DogAdoption/DogAdoption/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DogAdoption.Startup))]
+namespace DogAdoption
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
