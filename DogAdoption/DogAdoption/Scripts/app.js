95a52aa6b2dcaef59c32b550d60fc4ae76761c91
Callan Morrison <callan.l.morrison@gmail.com>
2016-12-05 18:58:19 -0600

    added geolocation based on webbrowser

--- DogAdoption/DogAdoption/Scripts/app.js
+++ DogAdoption/DogAdoption/Scripts/app.js
@@ -1,5 +1,26 @@
-﻿var app = angular.module('Companion', []);
+﻿"use strict";
+
+var app = angular.module('Companion', []);
 
 app.controller('Splash', function ($scope) {
 
+})
+
+app.factory('Geolocation', function ($q, $window) {
+    const GetLatAndLong = () => {
+        return $q(function (resolve, reject) {
+            if (!$window.navigator) {
+                reject(new Error('Geolocation is not supported'));
+            } else {
+                $window.navigator.geolocation.getCurrentPosition(function (position) {
+                    resolve({
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    });
+                }, reject(new Error('Geolocation is not returned.')));
+            }
+        })
+    }
+
+    return {GetLatAndLong}
 })
